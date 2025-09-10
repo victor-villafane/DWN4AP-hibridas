4b17e7c815c423ff38a47d394cb136b709b867ef
victor-villafane <victor.villafane@davinci.edu.ar>
2025-09-09 22:55:48 -0300

    mongodb

--- clase-09/api/routes/productos.api.routes.js
+++ clase-09/api/routes/productos.api.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import * as controllers from "../controllers/controller.api.productos.js"
 
 const route = Router()
-
+//query string mongodb+srv://admin:admin@dwn4ap.syxmi5s.mongodb.net/
 route.get("/", controllers.getProductos)
 route.get("/:id", controllers.getProductoById)
 route.post("/", controllers.nuevoProducto)
